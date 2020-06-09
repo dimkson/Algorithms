64fcf3bcd316220f56503ec4ecdbc24965e7bed0
dimkson <dimkson@yahoo.com>
2020-06-09 11:17:36 +0300

    Добавлен Урок 1.

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void Task01()
+        {
+            /*1. Ввести вес и рост человека. 
+             * Рассчитать и вывести индекс массы тела по формуле I = m / (h * h), где
+             * m – масса тела в килограммах, h – рост в метрах.*/
+
+        }
+    }
+
+}
