8abbfe0e1a5b4c6e6bc0be47ff94800d94ceb04c
dimkson <dimkson@yahoo.com>
2020-06-09 14:35:54 +0300

    добавлено задание 4,5

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -13,7 +13,8 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            Menu.delMenu[] delMenus = new Menu.delMenu[] { Task01,Task02, Task03 };
+            Menu.delMenu[] delMenus = new Menu.delMenu[] { Task01,Task02, Task03,
+                Task04, Task05};
             Menu menu = new Menu(delMenus);
             menu.ChooseMenu();
             //Type type = typeof(FastConsole);
@@ -60,6 +61,65 @@ namespace Lesson1
             b = a - b;
             a = a - b;
             Console.WriteLine($"Первое число: {a}, второе число: {b}");
+            FC.Pause();
+        }
+        static void Task04()
+        {
+            //4. Написать программу нахождения корней заданного квадратного уравнения.
+            FC.Input("Введите a", out int a);
+            FC.Input("Введите b", out int b);
+            FC.Input("Введите c", out int c);
+            Console.WriteLine($"Квадратное уравнение: {a}x^2+({b}x)+({c})=0");
+            double D = b * b - 4 * a * c;
+            double res1, res2;
+            if (D > 0)
+            {
+                res1 = (-b + Math.Sqrt(D)) / (2 * a);
+                res2 = (-b - Math.Sqrt(D)) / (2 * a);
+                Console.WriteLine($"Корни уравнения x1={res1}, x2={res2}");
+            }
+            else if(D==0)
+            {
+                res1 = -b / 2 * a;
+                Console.WriteLine($"Корень уравнения x={res1}");
+            }
+            else
+            {
+                Console.WriteLine("Уравнение не имеет корней");
+            }
+            FC.Pause();
+        }
+        static void Task05()
+        {
+            /*5. С клавиатуры вводится номер месяца.
+             * Требуется определить, к какому времени года он относится.*/
+            FC.Input("Введите номер месяца", out int month);
+            switch (month)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    Console.WriteLine("Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Осень");
+                    break;
+                default:
+                    Console.WriteLine("Необходимо ввести число от 1 до 12");
+                    break;
+            }
             FC.Pause();
         }
     }
