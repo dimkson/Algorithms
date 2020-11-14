1ec29987fc0aa98ea55f4365e15a3c5d191a575e
dimkson <dimkson@yahoo.com>
2020-11-14 15:42:42 +0300

    Реализована сортировка подсчетом.

--- Lesson8/Lesson8/Program.cs
+++ Lesson8/Lesson8/Program.cs
@@ -10,6 +10,41 @@ namespace Lesson8
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[50];
+            Random rnd = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                arr[i] = rnd.Next(0, 100);
+            }
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Task01(arr);
+            Console.ReadLine();
+        }
+        static void Task01(int[] arr)
+        {
+            //Реализовать сортировку подсчетом.
+
+            int max = arr[0];
+            foreach (int item in arr)
+            {
+                if (item > max) max = item;
+            }
+            int[] arrCount = new int[max + 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrCount[arr[i]]++;
+            }
+            for (int i = 0; i < arrCount.Length; i++)
+            {
+                for (int j = 0; j < arrCount[i]; j++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
         }
     }
 }
